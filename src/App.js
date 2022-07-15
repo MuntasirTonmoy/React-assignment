5b8d527c2251435d4aaad23acba5a89a1ce282dc
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-07-15 23:20:55 +0600

    error page added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import BookingForm from "./Components/BookingForm";
+import Error404 from "./Components/Error404";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import ShowDetails from "./Components/ShowDetails";
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="*" element={<Error404 />}></Route>
         <Route path="showdetails/:id" element={<ShowDetails />}></Route>
         <Route path="booking/:id" element={<BookingForm />}></Route>
       </Routes>
---
+++ src/Components/Error404.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Error404 = () => {
+  return (
+    <div className="h-[calc(100vh-72px)] flex justify-center items-center">
+      <div>
+        <h1 className="text-8xl font-bold text-center">Error 404</h1>
+        <p className="text-center my-5 text-2xl font-bold">Page Not Found.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Error404;
